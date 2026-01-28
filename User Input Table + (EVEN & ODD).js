da14d821e0614eabb9bfe0052c64822bb9e1f888
Aakash Sharma <akashshar9029@gmail.com>
2026-01-28 17:29:41 +0530

    Add user input tables for multiplication, even, and odd

---
+++ User Input Table + (EVEN & ODD).js
@@ -0,0 +1,20 @@
+1. *USER INPUT TABLE*
+
+let num = prompt("enter the value : ");
+
+    for(let i = 1; i<=10; i++){
+                   console.log(`${num} * ${i} = ${i*num} `);
+    }
+
+
+2. *USER INPUT EVEN TABLE*
+
+  let num = prompt("enter the value : ");
+
+    for(let i = 1; i<=10; i++){
+        if(i % 2 == 0){
+                console.log(`${num} * ${i} = ${i*num} `);
+     }
+    }
+
+3. *USER INPUT ODD TABLE*
