32b9be68759fc1121883c0881d822bc209267de3
Aakash Sharma <akashshar9029@gmail.com>
2026-01-28 17:46:37 +0530

    Add percentage calculation in JavaScript

---
+++ Percentage in javascript.js
@@ -0,0 +1,6 @@
+let obtained = (prompt("Enter obtained marks:"));
+let total = (prompt("Enter total marks:"));
+
+let percentage = (obtained / total) * 100;
+
+console.log("Percentage =", percentage + "%");
